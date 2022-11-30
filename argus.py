3147fc16ecb70938d7a25d68736a785fdf011fbf
azaustin21 <@GitZarePesh789>
2022-11-29 21:33:46 -0600

    Setup initial main, setup local database and grafana for visuals

---
+++ argus.py
@@ -0,0 +1,16 @@
+import logging
+import os
+import sys
+import requests
+from distutils.util import strtobool
+
+GITHUB_ORGANIZATIONS = []
+BITBUCKET_PROJECTS = []
+
+
+def main():
+    logging.info(f'Hello, Argus')
+
+
+if __name__ == '__main__':
+    main()
